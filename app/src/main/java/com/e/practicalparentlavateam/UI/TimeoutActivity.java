0b68e58891827c5ad97fdb7c7f37446d82bedea6
Sami PC <ahmad.as.sami07@gmail.com>
2020-10-29 04:51:50 +0600

    Got the basic timer set up.

--- app/src/main/java/com/e/practicalparentlavateam/UI/TimeoutActivity.java
+++ app/src/main/java/com/e/practicalparentlavateam/UI/TimeoutActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.e.practicalparentlavateam.R;
@@ -15,6 +19,30 @@ public class TimeoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeout);
+        final TextView timertext=findViewById(R.id.timertext);
+        int usertime=30000;
+
+
+        Button timeoutbtn=(Button)  findViewById(R.id.timepushbtn);
+
+        timeoutbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new CountDownTimer(30000, 1000) {
+
+                    public void onTick(long millisUntilFinished) {
+                        timertext.setText("Time Left " + millisUntilFinished / 1000);
+                    }
+
+                    public void onFinish() {
+                        timertext.setText("done!");
+                    }
+                }.start();
+            }
+        });
+
+
+
 
     }
     public static Intent makeIntent(Context context) {
