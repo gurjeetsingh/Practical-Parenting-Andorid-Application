8f74a9774ba95d76e0392c2fa93eb6654e73b9f3
Sami PC <ahmad.as.sami07@gmail.com>
2020-10-31 20:01:20 +0600

    Timer Working,trying to make a separate reset button

--- app/src/main/java/com/e/practicalparentlavateam/UI/TimeoutActivity.java
+++ app/src/main/java/com/e/practicalparentlavateam/UI/TimeoutActivity.java
@@ -13,41 +13,92 @@ import android.widget.Toast;
 
 import com.e.practicalparentlavateam.R;
 
+import java.util.Locale;
+
 public class TimeoutActivity extends AppCompatActivity {
+
+    private static final long START_TIME_IN_MILLIS = 600000;
+    private TextView mTextViewCountDown;
+    private Button mButtonStartPause;
+    private Button mButtonReset;
+    private CountDownTimer mCountDownTimer;
+    private boolean mTimerRunning;
+    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeout);
-        final TextView timertext=findViewById(R.id.timertext);
-        int usertime=30000;
-
-
-        Button timeoutbtn=(Button)  findViewById(R.id.timepushbtn);
-
-        timeoutbtn.setOnClickListener(new View.OnClickListener() {
+        mTextViewCountDown = findViewById(R.id.timertext);
+        mButtonStartPause = findViewById(R.id.timepushbtn);
+        mButtonReset = findViewById(R.id.resetbtn);
+        mButtonStartPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new CountDownTimer(30000, 1000) {
-
-                    public void onTick(long millisUntilFinished) {
-                        timertext.setText("Time Left " + millisUntilFinished / 1000);
-                    }
-
-                    public void onFinish() {
-                        timertext.setText("done!");
-                    }
-                }.start();
+                if (mTimerRunning) {
+                    pauseTimer();
+                } else {
+                    startTimer();
+                }
             }
         });
-
-
-
-
+        mButtonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetTimer();
+            }
+        });
+        updateCountDownText();
     }
+
+    private void startTimer() {
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                mTimerRunning = false;
+                mButtonStartPause.setText("Start");
+                mButtonStartPause.setVisibility(View.INVISIBLE);
+                mButtonReset.setVisibility(View.VISIBLE);
+            }
+        }.start();
+        mTimerRunning = true;
+        mButtonStartPause.setText("pause");
+        mButtonReset.setVisibility(View.INVISIBLE);
+    }
+
     public static Intent makeIntent(Context context) {
         Intent timeoutintent = new Intent(context, TimeoutActivity.class);
         return timeoutintent;
+    }
+
+
+    private void pauseTimer() {
+        mCountDownTimer.cancel();
+        mTimerRunning = false;
+        mButtonStartPause.setText("Start");
+        mButtonReset.setVisibility(View.VISIBLE);
+    }
+
+    private void resetTimer() {
+        mTimeLeftInMillis = START_TIME_IN_MILLIS;
+        updateCountDownText();
+        mButtonReset.setVisibility(View.INVISIBLE);
+        mButtonStartPause.setVisibility(View.VISIBLE);
+    }
+
+    private void updateCountDownText() {
+        int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
+        int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
+        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+        mTextViewCountDown.setText(timeLeftFormatted);
+
+
     }
 
 }
