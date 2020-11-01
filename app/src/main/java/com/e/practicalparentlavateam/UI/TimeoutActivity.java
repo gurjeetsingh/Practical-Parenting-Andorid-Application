1ad097b16e132b227d4ddb57359d0d62330bd998
Sami PC <ahmad.as.sami07@gmail.com>
2020-11-02 03:25:09 +0600

    Timer Almost Complete. Refactoring,reset button, alarm notification, perfecting timer left. Still very buggy.

--- app/src/main/java/com/e/practicalparentlavateam/UI/TimeoutActivity.java
+++ app/src/main/java/com/e/practicalparentlavateam/UI/TimeoutActivity.java
@@ -11,7 +11,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Binder;
 import android.os.Bundle;
@@ -19,6 +21,7 @@ import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Vibrator;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -28,49 +31,80 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
+import com.e.practicalparentlavateam.Model.AudioPlay;
+import com.e.practicalparentlavateam.Model.CounterService;
 import com.e.practicalparentlavateam.R;
 
 import java.util.Locale;
 
 public class TimeoutActivity extends AppCompatActivity {
 
-    private long START_TIME_IN_MILLIS = 600000;
+
     private TextView mTextViewCountDown;
     private Button mButtonStartPause;
     private Button mButtonReset;
     private Button custombtn;
+    private Button alrmoffbtn;
+    private long START_TIME_IN_MILLIS = 10000;
     EditText usertime;
     private CountDownTimer mCountDownTimer;
-    private boolean mTimerRunning;
-    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
-    private long mEndTime;
+    private boolean istimerrunning=false;
+    private long mTimeLeftInMillis;
     private long selectedtime;
-    final static int RQS_1 = 1;
-    final Context context=this;
+    Context context = this;
+    private final static String TAG = "BroadcastService";
     String[] timepiece = new String[]{"Select Duration", "Set Time: 1 Minute", "Set Time: 2 Minutes","Set Time: 3 Minutes","Set Time: 5 Minutes","Set Time: 10 Minutes"};
 
+
+    private Button startButton;
+    private Button pauseButton;
+
+
+    private TextView timerValue;
+
+    Intent intent;
+    Intent pauseintent;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeout);
-        mTextViewCountDown = findViewById(R.id.timertext);
-        mTextViewCountDown.setBackgroundResource(R.color.stopg);
-        mButtonStartPause = findViewById(R.id.timepushbtn);
-        custombtn=findViewById(R.id.customtime);
         createtimedurationspinner();
-        mButtonReset = findViewById(R.id.resetbtn);
-        mButtonStartPause.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mTimerRunning) {
-                    pauseTimer();
-                } else {
-                    startTimer();
-                }
+        timerValue = (TextView) findViewById(R.id.timertext);
+        timerValue.setBackgroundResource(R.color.stopg);
+        startButton = (Button) findViewById(R.id.timepushbtn);
+        startButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+
+                Intent serviceintent = new Intent(TimeoutActivity.this, CounterService.class);
+                serviceintent.putExtra("mills", mTimeLeftInMillis);
+                istimerrunning=true;
+                startService(serviceintent);
+                System.out.println("Time left for start"+mTimeLeftInMillis);
+                registerReceiver(broadcastReceiver, new IntentFilter(CounterService.BROADCAST_ACTION));
+                pauseButton.setVisibility(View.VISIBLE);
             }
         });
 
+        pauseButton = (Button) findViewById(R.id.pausebtn);
 
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                    int time = pauseintent.getIntExtra("time", 0);
+                    mTimeLeftInMillis = time;
+                //System.out.println("Time left for pause"+mTimeLeftInMillis);
+                    Intent serviceintent = new Intent(TimeoutActivity.this, CounterService.class);
+                    stopService(serviceintent);
+                   // unregisterReceiver(broadcastReceiver);
+                pauseButton.setVisibility(View.INVISIBLE);
+
+            }
+        });
+
+        custombtn= (Button) findViewById(R.id.customtime);
         custombtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -86,8 +120,6 @@ public class TimeoutActivity extends AppCompatActivity {
                         long customtime = Long.parseLong(newtime);
                         mTimeLeftInMillis=customtime*60000;
                         selectedtime=mTimeLeftInMillis;
-                        updateCountDownText();
-
                     }
                 });
                 builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -100,132 +132,18 @@ public class TimeoutActivity extends AppCompatActivity {
                 ad.show();
             }
         });
-        mButtonReset.setOnClickListener(new View.OnClickListener() {
+
+        alrmoffbtn=(Button) findViewById(R.id.alarmoff);
+
+        alrmoffbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                resetTimer();
+                AudioPlay.stopAudio();
             }
         });
-        updateCountDownText();
-    }
-
-    @Override
-    protected void onStop(){
-        super.onStop();
-        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putLong("millisLeft", mTimeLeftInMillis);
-        editor.putBoolean("timerRunning", mTimerRunning);
-        editor.putLong("endTime", mEndTime);
-        editor.apply();
-        if (mCountDownTimer != null) {
-            mCountDownTimer.cancel();
-        }
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
-        mTimeLeftInMillis = prefs.getLong("millisLeft", selectedtime);
-        mTimerRunning = prefs.getBoolean("timerRunning", false);
-        updateCountDownText();
-        updateButtons();
-        if (mTimerRunning) {
-            mEndTime = prefs.getLong("endTime", 0);
-            mTimeLeftInMillis = mEndTime - System.currentTimeMillis();
-            if (mTimeLeftInMillis < 0) {
-                mTimeLeftInMillis = 0;
-                mTimerRunning = false;
-                updateCountDownText();
-                updateButtons();
-            } else {
-                startTimer();
-            }
-        }
-    }
-
-
-    private void startTimer() {
-        mEndTime = System.currentTimeMillis() + mTimeLeftInMillis;
-        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                mTimeLeftInMillis = millisUntilFinished;
-                updateCountDownText();
-            }
-
-            @Override
-            public void onFinish() {
-                mTimerRunning = false;
-                mButtonStartPause.setText("Start");
-                resetTimer();
-
-
-               // mButtonStartPause.setVisibility(View.INVISIBLE);
-               // mButtonReset.setVisibility(View.VISIBLE);
-            }
-        }.start();
-        mTimerRunning = true;
-        mButtonStartPause.setText("pause");
-       // mButtonReset.setVisibility(View.INVISIBLE);
-    }
-
-    public static Intent makeIntent(Context context) {
-        Intent timeoutintent = new Intent(context, TimeoutActivity.class);
-        return timeoutintent;
-    }
-
-
-    private void pauseTimer() {
-        mCountDownTimer.cancel();
-        mTimerRunning = false;
-        mButtonStartPause.setText("Resume");
-        mButtonReset.setVisibility(View.VISIBLE);
-    }
-
-    private void resetTimer() {
-        mCountDownTimer.cancel();
-        mTimerRunning = false;
-        mTimeLeftInMillis = selectedtime;
-        mButtonStartPause.setText("Start");
-        updateCountDownText();
-       // mButtonReset.setVisibility(View.INVISIBLE);
-        //mButtonStartPause.setVisibility(View.VISIBLE);
-    }
-
-    private void updateCountDownText() {
-        int minutes = (int) (mTimeLeftInMillis / (double)1000) / 60;
-        int seconds = (int) (mTimeLeftInMillis / (double) 1000) % 60;
-        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
-        mTextViewCountDown.setText(timeLeftFormatted);
 
 
     }
-
-    private void updateButtons() {
-        if (mTimerRunning) {
-            //mButtonReset.setVisibility(View.INVISIBLE);
-            mButtonStartPause.setText("Pause");
-        } else {
-            mButtonStartPause.setText("Start");
-            /*
-            if (mTimeLeftInMillis < 1000) {
-                mButtonStartPause.setVisibility(View.INVISIBLE);
-            } else {
-                mButtonStartPause.setVisibility(View.VISIBLE);
-
-
-            }
-            if (mTimeLeftInMillis < START_TIME_IN_MILLIS) {
-                mButtonReset.setVisibility(View.VISIBLE);
-            } else {
-                mButtonReset.setVisibility(View.INVISIBLE);
-            }
-             */
-        }
-    }
-
 
     private void createtimedurationspinner() {
         Spinner timefieldspinner= (Spinner) findViewById(R.id.timespinner);
@@ -236,84 +154,81 @@ public class TimeoutActivity extends AppCompatActivity {
         timefieldspinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                if(position==0)
-                {
-                    if(mTimerRunning==true)
-                    {
-                       // Toast.makeText(TimeoutActivity.this, "Please Reset Timer First.", Toast.LENGTH_SHORT).show();
-                    }
-                    else {
-                        mTimeLeftInMillis = 60000;
-                        selectedtime = 60000;
-                        updateCountDownText();
-                    }
+                if (position == 0) {
+                    mTimeLeftInMillis = 60000;
+                    selectedtime = 60000;
+                    // Toast.makeText(TimeoutActivity.this, "Please Reset Timer First.", Toast.LENGTH_SHORT).show();
                 }
-                if(position==1)
-                {
-                    if(mTimerRunning==true)
-                    {
-                        Toast.makeText(TimeoutActivity.this, "Please Reset Timer First.", Toast.LENGTH_SHORT).show();
-                    }
-                    else {
-                        mTimeLeftInMillis = 60000;
-                        selectedtime = 60000;
-                        updateCountDownText();
-                    }
+                if (position == 1) {
+                    mTimeLeftInMillis = 60000;
+                    selectedtime = 60000;
                 }
-                if(position==2)
-                {
-                    if(mTimerRunning==true)
-                    {
-                        Toast.makeText(TimeoutActivity.this, "Please Reset Timer First.", Toast.LENGTH_SHORT).show();
-                    }
-                    else {
-                        mTimeLeftInMillis = 120000;
-                        selectedtime = 120000;
-                        updateCountDownText();
-                    }
+                if (position == 2) {
+                    mTimeLeftInMillis = 120000;
+                    selectedtime = 120000;
                 }
-                if(position==3) {
-                    if (mTimerRunning == true) {
-                        Toast.makeText(TimeoutActivity.this, "Please Reset Timer First.", Toast.LENGTH_SHORT).show();
-                    } else {
-                        mTimeLeftInMillis = 180000;
-                        selectedtime = 180000;
-                        updateCountDownText();
-                    }
+                if (position == 3) {
+                    mTimeLeftInMillis = 180000;
+                    selectedtime = 180000;
                 }
-                if(position==4)
-                {
-                    if(mTimerRunning==true)
-                    {
-                        Toast.makeText(TimeoutActivity.this, "Please Reset Timer First.", Toast.LENGTH_SHORT).show();
-                    }
-                    else
-                    {
-                        mTimeLeftInMillis=300000;
-                        selectedtime=300000;
-                        updateCountDownText();
-                    }
-
-                    }
-                if(position==5) {
-                    if (mTimerRunning == true) {
-                        Toast.makeText(TimeoutActivity.this, "Please Reset Timer First.", Toast.LENGTH_SHORT).show();
-                    }
-                    else {
-                        mTimeLeftInMillis = 600000;
-                        selectedtime = 600000;
-                        updateCountDownText();
-                    }
+                if (position == 4) {
+                    mTimeLeftInMillis = 300000;
+                    selectedtime = 300000;
+                }
+                if (position == 5) {
+                    mTimeLeftInMillis = 600000;
+                    selectedtime = 600000;
                 }
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                mTimeLeftInMillis=60000;
+                mTimeLeftInMillis = selectedtime;
             }
         });
-
-
     }
 
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //Intent serviceIntent = new Intent(this, CounterService.class);
+        //startService(serviceIntent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(broadcastReceiver, new IntentFilter(CounterService.BROADCAST_ACTION));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            updateUI(intent);
+        }
+    };
+
+    private void updateUI(Intent intent) {
+        int time = intent.getIntExtra("time", 0);
+        int mins = (int) (time/ (double)1000) / 60;
+        int secs = (int) (time / (double) 1000) % 60;
+
+        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", mins, secs);
+        timerValue.setText(timeLeftFormatted);
+        pauseintent=intent;
+    }
+
+
+
+    public static Intent makeIntent(Context context) {
+        Intent timeoutintent = new Intent(context, TimeoutActivity.class);
+        return timeoutintent;
+    }
 }
