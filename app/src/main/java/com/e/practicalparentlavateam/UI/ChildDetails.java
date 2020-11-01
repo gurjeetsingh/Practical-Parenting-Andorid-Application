04e201fe24a882840fdafa7e00c30abccbe47f05
gurjeet <gsg27@sfu.ca>
2020-11-01 15:27:30 -0800

    merged master to branch 3 added child details accivity after messup

--- app/src/main/java/com/e/practicalparentlavateam/UI/ChildDetails.java
+++ app/src/main/java/com/e/practicalparentlavateam/UI/ChildDetails.java
@@ -3,31 +3,32 @@ package com.e.practicalparentlavateam.UI;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.EditText;
-
-import androidx.appcompat.widget.Toolbar;
 
 import com.e.practicalparentlavateam.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
-public class ChildDetailsActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.EditText;
+
+public class ChildDetails extends AppCompatActivity {
     private EditText etName;
 
     public static Intent makeIntentForAdd(Context context) {
-        return new Intent(context, ChildDetailsActivity.class);
+        return new Intent(context, ChildDetails.class);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lens_details);
+        setContentView(R.layout.activity_child_details3);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        etMake = findViewById(R.id.etMake);
-        etAperture = findViewById(R.id.etAperture);
-        etFocalLength = findViewById(R.id.etFocalLength);
+        etName = findViewById(R.id.)
 
-        setupButtonCancel();
-        setupButtonOk();
     }
 }
