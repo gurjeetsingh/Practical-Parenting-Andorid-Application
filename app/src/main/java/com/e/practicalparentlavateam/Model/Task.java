ffe2aa87eeed5b3bc5f16ac3eff624a2706e5766
yha181 <yha181@sfu.ca>
2020-11-22 14:15:38 -0800

    Refactor the variable

--- app/src/main/java/com/e/practicalparentlavateam/Model/Task.java
+++ app/src/main/java/com/e/practicalparentlavateam/Model/Task.java
@@ -4,16 +4,17 @@ public class Task {
     private String task;
     private String name;
 
-    public void setTask(String t){ task = t;}
+    public void setTask(String task){ this.task = task;}
 
-    public void setName(String n){name = n;}
+    public void setName(String name){
+        this.name = name;}
 
     public String getTask() {return task;}
 
     public String getName(){return name;}
 
-    public Task(String time, String name){
-        setTask(time);
+    public Task(String task, String name){
+        setTask(task);
         setName(name);
     }
 }
