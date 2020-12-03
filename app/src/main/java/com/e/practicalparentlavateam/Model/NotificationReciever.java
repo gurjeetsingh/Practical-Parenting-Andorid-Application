92b76be128f6dff31733f2be961c20fbfe6e7719
Sami PC <ahmad.as.sami07@gmail.com>
2020-12-03 20:39:33 +0600

    Negative time -1 for 100% time is fixed. Some refactoring

--- app/src/main/java/com/e/practicalparentlavateam/Model/NotificationReciever.java
+++ app/src/main/java/com/e/practicalparentlavateam/Model/NotificationReciever.java
@@ -8,6 +8,9 @@ import android.content.Intent;
 import androidx.core.app.NotificationManagerCompat;
 
 import com.e.practicalparentlavateam.UI.TimeoutActivity;
+/*
+This class recieves the clickback on the notification, and when clicked, stops the audio.
+ */
 
 public class NotificationReciever extends BroadcastReceiver {
     @Override
